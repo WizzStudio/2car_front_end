8ef4eed89508f5afa0909bee0620dabde9f49148
CWsuper <845202636@qq.com>
2017-12-09 16:40:16 +0800

    login finish

--- script/login.js
+++ script/login.js
@@ -1,27 +1,32 @@
 var baseUrl = '/apis'; 
 $("#submit").click(function(){
     console.log("aa");
-                    var username = $("#username").val();
-                    var password = $("#password").val();
-                    if(username == '' || password == ''){
-                        alert("请输入账号密码");
-                    }else{
-                        $.ajax({
-                            url:'',
-                            type:'post',
-                            data:{
-                                username:username,
-                                password:password
-                            },
-                            dataType:'json',
-                            success:function(data){
-                                alert(data.msg);                                     
-                                 window.location.href="./backstageIndex.html"; 
-                                 return false;
-                            },
-                            error:function(error){
-                                alert(error.msg);     
-                            }
-                        })
-                    }
-                });
+    var username = $("#username").val();
+    var password = $("#password").val();
+    if(username == '' || password == ''){
+        alert("请输入账号密码");
+    }else{
+        $.ajax({
+            url:baseUrl+'/SecondCar/tg_managerAction.action',
+            type:'post',
+            data:{
+                username:username,
+                password:password
+            },
+            dataType:'json',
+            success:function(data){
+                if(data.code == 1){
+                    alert(data.msg);                                     
+                    window.location.href="./backstageIndex.html"; 
+                    return false;
+                }else{
+                    alert(data.msg);                                                                         
+                }
+                
+            },
+            error:function(error){
+                alert("网络请求错误");     
+            }
+        })
+    }
+});
