8d2293195ec6110e0b7a4c1d1fae43052bc77c8a
CWsuper <845202636@qq.com>
2017-12-09 13:17:50 +0800

    .

--- script/login.js
+++ script/login.js
@@ -1,12 +1,13 @@
 var baseUrl = '/apis'; 
 $("#submit").click(function(){
+    console.log("aa");
                     var username = $("#username").val();
                     var password = $("#password").val();
                     if(username == '' || password == ''){
                         alert("请输入账号密码");
                     }else{
                         $.ajax({
-                            url:'mmm',
+                            url:'',
                             type:'post',
                             data:{
                                 username:username,
@@ -18,9 +19,7 @@ $("#submit").click(function(){
                                  return false;
                             },
                             error:function(error){
-                                // alert(error.msg);       
-                                window.location.href="./backstageIndex.html";
-                                return false;
+                                alert(error.msg);     
                                 
                             }
                         })
